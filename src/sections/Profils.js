d4929f3cb40aaf616c8e9f90ee3918f8d8e25abb
Pierre <gordepierre@gmail.com>
2020-06-05 13:25:41 +0200

    UPDATE > width card profil

--- src/sections/Profils.js
+++ src/sections/Profils.js
@@ -197,7 +197,7 @@ const Profils = () => (
         }
       `}
       render={({ contentfulAbout }) => (
-        <CardContainer minWidth="50vw">
+        <CardContainer minWidth="40vw">
           {contentfulAbout.profils.map((p, i) => (
             <Fade bottom delay={i * 200} key={p.id}>
               <Profil {...p} />
